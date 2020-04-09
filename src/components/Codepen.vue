d4dd03aeb438246f587708e8f59a1758fb768186
ryanzola <ryanzola@me.com>
2020-04-08 23:16:08 -0400

    Codepen custom fields

---
+++ src/components/Codepen.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="pen">
+    <h2 v-html="pen.title" />
+    <img :src="pen.featuredMedia.sourceUrl" alt="">
+    <a :href="pen.acf.codepenUrl">codepen link</a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pen: {
+      type: Object,
+      required: true
+    }
+  },
+  mounted() {
+    console.log(this.pen)
+  }
+}
+</script>
+
+<style scoped>
+.pen {
+  border: 1px solid #ccc;
+  border-radius: 0.25rem;
+  padding: 1rem;
+  background-color: #f5f6f8;
+}
+
+h2 {
+  color: #343537;
+  margin-top: 0;
+}
+</style>
---
+++ src/pages/Codepen.vue
@@ -0,0 +1,46 @@
+<template>
+  <Layout>
+    <h1>Welcome to my codepens :)</h1>
+    <ul class="post-list">
+      <li v-for="{ node } in $page.allWordPressCodepen.edges" :key="node.id">
+        <Codepen :pen="node" />
+      </li>
+    </ul>
+
+  </Layout>
+</template>
+
+<page-query>
+query Codepen ($page: Int) {
+  allWordPressCodepen (page: $page, perPage: 10) @paginate {
+    edges {
+      node {
+        title
+        id
+        featuredMedia {
+          sourceUrl
+        }
+        acf {
+          codpenDescription
+          codepenUrl
+        }
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+import { Pager } from 'gridsome'
+import Codepen from '~/components/Codepen.vue'
+
+export default {
+  components: {
+    Pager,
+    Codepen
+  },
+  metaInfo: {
+    title: 'Welcome to my blog :)'
+  }
+}
+</script>
